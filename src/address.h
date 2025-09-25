66f1945f75aabb1d09720fcc9bbed5231d3f6b97
takumichronen <kostaslia08@gmail.com>
2025-09-25 19:22:20 +0300

    Update address.h

--- src/address.h
+++ src/address.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <cstdint>
+
 namespace miq {
 bool decode_p2pkh_address(const std::string& addr, std::vector<uint8_t>& out_pkh);
 std::string encode_p2pkh_address(const std::vector<uint8_t>& pkh);
