c3ba2bcfec2fde9ece8c3446a766190246e86544
takumichronen <kostaslia08@gmail.com>
2025-09-27 19:12:49 +0300

    Update chain.h

--- src/chain.h
+++ src/chain.h
@@ -16,6 +16,18 @@
 
 namespace miq {
 
+static inline std::string hk(const std::vector<uint8_t>& h){
+    return std::string(reinterpret_cast<const char*>(h.data()), h.size());
+}
+
+static std::unordered_map<std::string, std::vector<UndoIn>> g_undo;
+
+struct UndoIn {
+    std::vector<uint8_t> prev_txid;
+    uint32_t             prev_vout{0};
+    UTXOEntry            prev_entry;
+};
+
 struct Tip {
     uint64_t             height;
     std::vector<uint8_t> hash;
