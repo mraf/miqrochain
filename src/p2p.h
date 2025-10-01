ea0f9a7ec07dc0094b1bc4d096c1f91b2676d95f
takumichronen <kostaslia08@gmail.com>
2025-10-01 05:33:31 +0300

    Update p2p.h

--- src/p2p.h
+++ src/p2p.h
@@ -154,6 +154,7 @@ public:
     inline Mempool*       mempool()       { return mempool_; }
     inline const Mempool* mempool() const { return mempool_; }
 
+    bool check_rate(PeerState& ps, const char* key);
     bool start(uint16_t port);
     void stop();
 
