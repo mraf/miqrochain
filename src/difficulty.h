a21300df35616159baeb48f854f5babf6aebf0f5
takumichronen <kostaslia08@gmail.com>
2025-10-01 04:10:23 +0300

    Update difficulty.h

--- src/difficulty.h
+++ src/difficulty.h
@@ -13,4 +13,4 @@ uint32_t lwma_next_bits(const std::vector<std::pair<int64_t,uint32_t>>& last,
                         int64_t target_spacing,
                         uint32_t min_bits);
 
-} // namespace miq
+}
