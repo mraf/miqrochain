f447f39aba3b64c16204bbe5e8c1f56faf0f3140
takumichronen <kostaslia08@gmail.com>
2025-10-02 15:48:38 +0300

    Create overviewwidget.cpp

---
+++ gui/miqro_gui/src/widgets/overviewwidget.cpp
@@ -0,0 +1,53 @@
+#include "overviewwidget.h"
+#include "../rpcclient.h"
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QTimer>
+#include <QJsonObject>
+
+
+class KV : public QWidget {
+Q_OBJECT
+public:
+KV(const QString &k, const QString &v, QWidget *p=nullptr) : QWidget(p){
+auto *l = new QHBoxLayout(this);
+l->addWidget(new QLabel(QString("<b>%1</b>").arg(k)));
+m_v = new QLabel(v); l->addWidget(m_v, 1);
+}
+void setV(const QString &v){ m_v->setText(v); }
+private:
+QLabel *m_v{nullptr};
+};
+
+
+OverviewWidget::OverviewWidget(RpcClient &rpc, QWidget *p) : QWidget(p), m_rpc(rpc){
+auto *lay = new QVBoxLayout(this);
+auto *h = new QLabel("<h2>Overview</h2>"); lay->addWidget(h);
+
+
+auto *hHeight = new KV("Height", "-");
+auto *hHash = new KV("Best Hash", "-");
+auto *hDiff = new KV("Bits", "-");
+
+
+lay->addWidget(hHeight);
+lay->addWidget(hHash);
+lay->addWidget(hDiff);
+lay->addStretch(1);
+
+
+m_timer = new QTimer(this);
+m_timer->setInterval(2000);
+connect(m_timer, &QTimer::timeout, this, [this, hHeight, hHash, hDiff]{
+try {
+const auto v = m_rpc.getTipInfo().toObject();
+hHeight->setV(QString::number(v.value("height").toInt()));
+hHash->setV(v.value("best_hash").toString());
+hDiff->setV(QString::number(v.value("bits").toInt()));
+} catch (...) {}
+});
+m_timer->start();
+}
+
+
+#include "overviewwidget.moc"
