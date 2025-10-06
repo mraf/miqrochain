d6090010b522ad643633ce8e51786320493d136f
takumichronen <kostaslia08@gmail.com>
2025-10-06 23:51:49 +0300

    Update chain.cpp

--- src/chain.cpp
+++ src/chain.cpp
@@ -809,7 +809,7 @@ bool Chain::verify_block(const Block& b, std::string& err) const{
 
     // Coinbase shape
     const auto& cb = b.txs[0];
-    if (cb.vin.size()!=1 || !cb.vin[0].sig.empty() || !cb.vin[0].pubkey.empty()) { err="bad coinbase"; return false; }
+    if (cb.vin.size()!=1 || !cb.vin[0].pubkey.empty()) { err="bad coinbase"; return false; }
     if (cb.vin[0].prev.txid.size()!=32) { err="bad coinbase prev size"; return false; }
     if (std::any_of(cb.vin[0].prev.txid.begin(), cb.vin[0].prev.txid.end(), [](uint8_t v){ return v!=0; })) { err="bad coinbase prev"; return false; }
     if (cb.vin[0].prev.vout != 0) { err="bad coinbase vout"; return false; }
